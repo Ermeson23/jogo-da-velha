62d4616bf3d37c0e9dc315ad1b6b5d0de32b87ac
Ermeson José Ribeiro <99134581+Ermeson23@users.noreply.github.com>
2022-12-16 21:48:07 -0300

    Add files via upload

---
+++ jogo-da-velha/js/scripts.js
@@ -0,0 +1,259 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageTex = document.querySelector("#message p");
+let secondPlayer;
+
+//Contador de Jogadas
+let player1 = 0;
+let player2 = 0;
+
+//Adicionando o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++) {
+
+    //Quando alguém clica na caixa
+    boxes[i].addEventListener("click", function() {
+
+        let el = checkEl(player1, player2);
+
+        //Verifica se já tem x ou o
+        if(this.childNodes.length == 0) {
+
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            //Computar a jogada
+            if(player1 == player2) {
+                player1++;
+
+                if(secondPlayer == 'ai-player') {
+                    
+                    //Função executar a jogada
+                    computerPlayer();
+
+                    player2++;
+                }
+
+            } else {
+                player2++;
+            }
+
+            //Checa Quem Venceu
+            checkWinCondition();
+
+        }
+
+    });
+}
+
+//Evento para Saber quem será o oponente: outro Jogador ou IA
+for(let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+        secondPlayer = this.getAttribute("id");
+
+        for(let j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function() {
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+        }, 300);
+
+    });
+}
+
+//Vê quem vai jogar
+function checkEl (player1, player2) {
+    (player1 == player2) ? el = x : el = o;
+    return el;
+}
+
+//Vê quem venceu a partida
+function checkWinCondition () {
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+
+    //Verificação nas Horizontais
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+
+        if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x') {
+            declareWinner('x');
+        } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0) {
+        let b4Child = b4.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+
+        if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x') {
+            declareWinner('x');
+        } else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b7Child = b7.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x') {
+            declareWinner('x');
+        } else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    //Verificação nas Verticais
+    if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b4Child = b4.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if(b1Child == 'x' && b4Child == 'x' && b7Child == 'x') {
+            declareWinner('x');
+        } else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0) {
+        let b2Child = b2.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+
+        if(b2Child == 'x' && b5Child == 'x' && b8Child == 'x') {
+            declareWinner('x');
+        } else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b3Child = b3.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b3Child == 'x' && b6Child == 'x' && b9Child == 'x') {
+            declareWinner('x');
+        } else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    //Verificação nas Diagonais
+    if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b1Child == 'x' && b5Child == 'x' && b9Child == 'x') {
+            declareWinner('x');
+        } else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0) {
+        let b3Child = b3.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if(b3Child == 'x' && b5Child == 'x' && b7Child == 'x') {
+            declareWinner('x');
+        } else if(b3Child == 'o' && b5Child == 'o' && b7Child == 'o') {
+            declareWinner('o');
+        }
+    }
+
+    //Deu velha
+    let counter = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+        if(boxes[i].childNodes[0] != undefined) counter++;
+    }
+
+    if (counter == 9) declareWinner();
+
+    function declareWinner(winner) {
+        let scoreboardX = document.querySelector("#scoreboard-1");
+        let scoreboardO = document.querySelector("#scoreboard-2");
+        let message ='';
+
+        if(winner == 'x') {
+            scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
+            message = "O Jogador 1 venceu!";
+        } else if(winner == 'o') {
+            scoreboardO.textContent = parseInt(scoreboardO.textContent) + 1;
+            message = "O Jogador 2 venceu!";
+        } else message = "Deu velha!";
+
+        //Exibe Mensagem
+        messageTex.innerHTML = message;
+        messageContainer.classList.remove("hide");
+
+        //Esconde Mensagem
+        setTimeout(function() {
+            messageContainer.classList.add("hide");
+        }, 1000);
+
+        //Zera as Jogadas
+        player1 = 0;
+        player2 = 0;
+
+        //Remove X e O
+        let boxesToRemove = document.querySelectorAll(".box div");
+
+        for(let i = 0; i < boxesToRemove.length; i++) {
+            boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+        }
+
+    }
+
+}
+
+//Executar a lógica da IA
+function computerPlayer() {
+
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i = 0; i < boxes.length; i++)  {
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        //Preenche se o filho estiver vazio
+        if(boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+            //Checa quantas casas preechidas
+        } else {
+            filled++;
+        }
+    }
+
+    if(counter == 0 && filled < 9) {
+        computerPlayer();
+    }
+}
